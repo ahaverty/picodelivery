48be2f28be494ba0c6ee0c4b2c72a0e9f5381fe5
Alan Haverty <alan@ahaverty.com>
2016-02-13 19:18:19 +0000

    Updating setup with module folder changes

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(
     name='PicoDelivery',
     version='0.1dev',
-    packages=['picodelivery','predictions','simulators'],
+    packages=['picodelivery','configuration'],
 )
