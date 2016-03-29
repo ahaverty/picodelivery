fc6abb1efc39cf9c30a0f6e7c421ae374fe996d7
Alan Haverty <alan@ahaverty.com>
2016-03-29 21:16:12 +0100

    Fixing bug with simulator modifying real users jobs.

--- configuration/simulators_sql.py
+++ configuration/simulators_sql.py
@@ -64,12 +64,14 @@ updateJobStatusesAndTimes = "SET @DRIVER_MIN = 36; SET @DRIVER_MAX = 345; " \
                             "DATE_ADD(start_time, INTERVAL (ROUND(RAND() * (@MAX_COLLECT - @MIN_COLLECT) + @MIN_COLLECT)) SECOND), " \
                             "status_id = 3 " \
                             "WHERE status_id = 2 " \
-                            "AND start_time < DATE_SUB(now(), INTERVAL @MAX_COLLECT SECOND); " \
+                            "AND start_time < DATE_SUB(now(), INTERVAL @MAX_COLLECT SECOND) " \
+                            "AND driver_id BETWEEN @DRIVER_MIN AND @DRIVER_MAX; " \
                             "UPDATE job " \
                             "SET end_time = " \
                             "DATE_ADD(payload_collected_time, INTERVAL (ROUND(RAND() * (@MAX_DELIVER - @MIN_DELIVER) + @MIN_DELIVER)) SECOND), " \
                             "status_id = 4 " \
-                            "WHERE status_id = 3 " \
+                            "WHERE status_id = 3  " \
+                            "AND driver_id BETWEEN @DRIVER_MIN AND @DRIVER_MAX " \
                             "AND payload_collected_time < DATE_SUB(now(), INTERVAL @MAX_DELIVER SECOND);"
 
 getAreaJobRateFromSimAgg = "SELECT count_of_jobs/(60/%s) as rate, area_id " \
